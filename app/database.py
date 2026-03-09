14d6c593f523fff3eb81925c9115ad5596377fb2
pikitosdoritos <fastovetsn@gmail.com>
2026-03-09 12:33:55 +0200

    initialize database connection and environment configuration

---
+++ app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_URL = os.getenv("DB_URL")
+
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal
+    try:
+        yield db
+    finally:
+        db.close() 
